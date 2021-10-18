8e19e4af865066f69fe14519864b3108771d350b
Jonathan Hussey <jhusseydesign@gmail.com>
2021-10-18 06:28:33 +0100

    Added documentation to  the SeedInitialiser

--- Opticverge.Evolution.Core/Generators/SeedInitialiser.cs
+++ Opticverge.Evolution.Core/Generators/SeedInitialiser.cs
@@ -3,12 +3,23 @@ using System.Threading;
 
 namespace Opticverge.Evolution.Core.Generators
 {
+    /// <summary>
+    /// Provides a threadsafe implementation for retrieveing a unique
+    /// seed that is incremented when a new seed is requested.
+    /// </summary>
     public static class SeedInitialiser
     {
         private static ulong _currentSeed = (ulong)Guid.NewGuid().GetHashCode();
 
+        /// <summary>
+        /// Returns the current seed.
+        /// </summary>
         public static ulong Seed => _currentSeed;
 
+        /// <summary>
+        /// Returns the current seed incremented by one.
+        /// </summary>
+        /// <returns></returns>
         public static ulong GetSeed()
         {
             return Interlocked.Add(ref _currentSeed, 1UL);
